61b5e2b202038d1c84389447280ed3d029595861
caiocroci <caiocroci@gmail.com>
2020-06-26 19:54:23 -0300

    jogo completo

---
+++ Assets/Scripts/EventLook.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EventLook : MonoBehaviour
+{
+	public GameObject bait;
+	public GameObject menu;
+	public int number;
+
+	public void ChangeScene()
+	{
+		if (bait)
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + number);
+		}
+
+		if (menu)
+		{
+			SceneManager.LoadScene("MENU");
+		}
+
+	}
+}
